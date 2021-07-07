5ad84066d86fa734a0548db19a148e209eb20ed4
rielyalkov <58986485+rielyalkov@users.noreply.github.com>
2021-07-07 15:30:07 +0300

    init

---
+++ app/index.js
@@ -0,0 +1,16 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'scss/_index.scss';
+import Genetic from 'genetic-js';
+
+
+console.log(document.querySelectorAll('div'));
+
+// ================================
+// START YOUR APP HERE
+// ================================
+
+console.log(Genetic);
